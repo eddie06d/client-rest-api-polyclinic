9177a1e8fff8ac3426430e4cd9776a0f48421039
eddie06 <ejhuancahuire@gmail.com>
2022-06-07 23:14:56 -0500

    final changes

---
+++ src/app/services/especialidades.service.ts
@@ -0,0 +1,31 @@
+import { Especialidad } from './../models/especialidad.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EspecialidadesService {
+  URL_API_PROD: string = 'https://policlinico.herokuapp.app/api/especialidades';
+  URL_API_DEV: string = 'http://localhost:4000/api/especialidades';
+
+  constructor(private http: HttpClient) { }
+
+  getEspecialidades(): Observable<any> {
+    return this.http.get<any>(this.URL_API_DEV);
+  }
+
+  createEspecialidad(data: Especialidad) {
+    return this.http.post(this.URL_API_DEV, data);
+  }
+
+  updateEspecialidad(id: string, data: Especialidad) {
+    return this.http.put(`${this.URL_API_DEV}/${id}`, data);
+  }
+
+  deleteEspecialidad(id: string) {
+    return this.http.delete(`${this.URL_API_DEV}/${id}`);
+  }
+
+}
